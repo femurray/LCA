f401980f06f7ec7f7fc6c8ffbcbb4610986aca20
femurray <femurray@tcd.ie>
2020-10-28 15:18:54 +0000

    renamed java files

--- LCA/src/LCABinaryTreeTest.java
+++ LCA/src/LCABinaryTreeTest.java
@@ -2,7 +2,7 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
-public class LCATest {
+public class LCABinaryTreeTest {
 
 	//idk what point of @test is - find out
 	public void testLCAOutput() 
