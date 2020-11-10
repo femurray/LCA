9549178946864b1ed94c167ecf3c53e0e71a8819
femurray <femurray@tcd.ie>
2020-11-10 14:50:05 +0000

    Implemented addEdge Method. Created new method validateVertex.

--- LCA/src/DAG.java
+++ LCA/src/DAG.java
@@ -16,21 +16,34 @@ public class DAG {
 	}
 
 	//Adds directed edge from v to w
-	public void addEdge(int V, int W)
+	public void addEdge(int v, int w)
 	{
-		
+		validateVertex(v);
+		validateVertex(w);
+		adj[v].add(w);
+		indegree[w]++;
+		E++;
+	}
+	
+	private void validateVertex(int v)
+	{
+		if(v < 0 || v >= V)
+		{
+			throw new IllegalArgumentException("Vertex " + v + " is not between 0 and " + (V-1));
+		}
 	}
 
 	//Returns number of directed edges from current vertex
-	public int indegree(int V)
+	public int indegree(int v)
 	{
+		validateVertex(v);
 		return indegree[V];
 	}
 
 	//Returns the adjacent vertices to v
-	public Iterable<Integer> adj(int V)
+	public Iterable<Integer> adj(int v)
 	{
-		return adj[V];
+		return adj[v];
 	}
 
 }
